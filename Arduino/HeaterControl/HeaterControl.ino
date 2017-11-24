1f72a2843cfee2074fe33bc729cded7c36371294
Palonen <ppalonen@bethel.jw.org>
2017-11-24 14:38:24 +0100

    Finished version with Display

--- Arduino/HeaterControl/HeaterControl.ino
+++ Arduino/HeaterControl/HeaterControl.ino
@@ -1,24 +1,70 @@
-
+//#define DEBUG
 
 #define OVERSAMPLENR 	16
 
 #define MOSFET_PIN 		A1
 #define TEMPSENSOR_PIN 	A0
-#define KNOB_PIN 		A2	// Potentsiomenter for setting the desired temperature
+#define KNOB_PIN 		A2	// Potentiometer for setting the desired temperature
 
 #include <U8g2lib.h>
 #include "temptable.h"
 
 static void* heater_ttbl_map = (void*)TEMPTABLE;
 
-U8G2_SSD1306_96X16_ER_F_HW_I2C u8g2(U8G2_R0);   // EastRising 0.69"
-
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
+//U8G2_SSD1306_96X16_ER_F_HW_I2C u8g2(U8G2_R0);   // EastRising 0.69"
 
 // ADC=1023 - temp sensor is not connected
 
 // mosfet (HIGH - OFF, LOW - ON)
 #define H_ON 	digitalWrite(MOSFET_PIN,LOW)
 #define H_OFF 	digitalWrite(MOSFET_PIN,HIGH)
+
+static const uint8_t u8g_clear_bits[] U8X8_PROGMEM = {
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+	0b00000000
+};
+
+static const uint8_t u8g_logo_bits[] U8X8_PROGMEM = {
+	0b10010000,
+	0b01001000,
+	0b00100100,
+	0b00100100,
+	0b01001000,
+	0b10010000,
+	0b10010000,
+	0b01001000,
+	0b00100100,
+	0b00100100,
+	0b00000000,
+	0b11111110,
+	0b00000000,
+	0b11111110
+};
+
+bool stateHeater;
+bool stateAdjustment=HIGH;	// Is temperature adjusted?
+int presetTemp;
+float currentTemp;
+int last_presetTemp = 0;
+unsigned long disp_hyst_ms = 0;
+unsigned long adjust_hyst_ms = 0;
+unsigned long switch_mosfet_hyst_ms = 0;
+#ifdef DEBUG
+unsigned long serial_mosfet_hyst_ms = 0;
+#endif
 
 void setup(){
 	
@@ -27,53 +73,57 @@ void setup(){
 	
 	u8g2.begin();
 	
+	#ifdef DEBUG
 	Serial.begin(57600);
+	#endif
 	
+	u8g2.clearBuffer();
+	u8g2.setFont(u8g2_font_inb16_mr);
+
 	// test temoerature sensor presense
-	while (analog2temp(collectADCraw(TEMPSENSOR_PIN))>999.0) {
-		delay(1000);
-	}
+	//while (analog2temp(collectADCraw(TEMPSENSOR_PIN))>999.0) {
+	//	delay(1000);
+	//}
+	#ifdef DEBUG
 	Serial.print("Temperature Sensor: ");
 	Serial.println(analog2temp(collectADCraw(TEMPSENSOR_PIN)));
+	#endif
 	
 }
 
-int last_presetTemp = 0;
-unsigned long disp_hyst_ms = 0;
-unsigned long adjust_hyst_ms = 0;
-unsigned long switch_mosfet_hyst_ms = 0;
-unsigned long serial_mosfet_hyst_ms = 0;
-
 void loop() {
 	
-	int presetTemp = map(collectADCraw(KNOB_PIN),0,1023*OVERSAMPLENR,20,300);
-	float currentTemp = analog2temp(collectADCraw(TEMPSENSOR_PIN));
+	presetTemp = map(collectADCraw(KNOB_PIN),0,1023*OVERSAMPLENR,20,300);
+	currentTemp = analog2temp(collectADCraw(TEMPSENSOR_PIN));
 
+	// Switch Heater ON/OFF
 	if (switch_mosfet_hyst_ms+500 < millis()) {
 		// if preset temperature is near 20, consider allways OFF
-		if ((float)presetTemp>=currentTemp && presetTemp>21) {H_ON;} else {H_OFF;}
+		if ((float)presetTemp>=currentTemp && presetTemp>21) {
+			if(!stateHeater){H_ON;stateHeater=HIGH;screenRedraw();}
+		} else {
+			if(stateHeater){H_OFF;stateHeater=LOW;screenRedraw();}
+		}
 		switch_mosfet_hyst_ms = millis();
 	}	
 
+	// Adjust temperature
+	if (abs(presetTemp-last_presetTemp)>2) {
+		stateAdjustment=HIGH;
+		adjust_hyst_ms = millis();
+		last_presetTemp = presetTemp;
+	} else {
+		if(adjust_hyst_ms+2000 < millis()){stateAdjustment=LOW;}
+	}
+	
+	// Display
 	if (disp_hyst_ms+700 < millis()) {
 		disp_hyst_ms=millis();
-		u8g2.clearBuffer();
-		u8g2.setFont(u8g2_font_inb16_mr);
-		u8g2.setCursor(0, 15);
-		if (presetTemp < (last_presetTemp-2) || presetTemp > (last_presetTemp+2)) { // add small hysterysis
-			// value is adjusted
-			last_presetTemp = presetTemp;
-			adjust_hyst_ms = millis();
-			u8g2.print("<");
-			u8g2.print(presetTemp);
-			u8g2.print(">");
-		} else if(adjust_hyst_ms+2000 < millis()){
-			u8g2.print(currentTemp);
-		}
-		u8g2.sendBuffer();
+		screenRedraw();
 	}	
 	
 	// debug 
+	#ifdef DEBUG
 	if (serial_mosfet_hyst_ms+1000 < millis()) {
 		serial_mosfet_hyst_ms = millis();
 		Serial.print("Set: ");
@@ -81,7 +131,23 @@ void loop() {
 		Serial.print(", Actual: ");
 		Serial.println(currentTemp);
 	}	
+	#endif
 
+}
+
+void screenRedraw() {
+	u8g2.clearBuffer();
+	if(stateHeater){u8g2.drawXBMP( 86, 1, 8, 14, u8g_logo_bits);}
+	u8g2.setCursor(0, 16);
+	if (stateAdjustment) {
+		// value is adjusted
+		u8g2.print("<");
+		u8g2.print(presetTemp);
+		u8g2.print(">");
+	} else {
+		u8g2.print(currentTemp,1);
+	}
+	u8g2.sendBuffer();
 }
 
 #define PGM_RD_W(x)   (short)pgm_read_word(&x)
@@ -98,7 +164,9 @@ int collectADCraw(uint8_t sensor) {
 
 float analog2temp(int raw) {
 	if (raw==1023) {
+		#ifdef DEBUG
 		Serial.println("No Sensor!");
+		#endif
 		return 999.9;
 	}
     float celsius = 0;
